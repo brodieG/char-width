29074c6d39507944dccd51372171d325450fa7df
brodieG <brodieG@users.noreply.github.com>
2020-04-04 19:58:51 -0400

    move funs to lib file

--- process-ea.R
+++ process-ea.R
@@ -11,133 +11,14 @@
 # Read the EastAsianWidth.txt table into a data.frame (From Gábor),
 # first part:
 
-options(stringsAsFactors=FALSE)
-udat <- read.delim(
-  stringsAsFactors = FALSE,
-  "EastAsianWidth.txt",
-  comment.char = "#",
-  sep = ";",
-  strip.white = TRUE,
-  header = FALSE
-)
-# Generate and validate the ranges
+source('process-lib.R')
 
-points <- strsplit(udat[['V1']], '..', fixed=TRUE)
-lens <- lengths(points)
-stopifnot(all(lens > 0 & lens < 3))
-for(i in seq_along(points)) length(points[[i]]) <- 2
-points <- matrix(unlist(points), 2)
+udat <- uni_eaw("EastAsianWidth.txt")
 
-udat[['start']] <- points[1,]
-udat[['end']] <- points[2,]
-udat <- within(udat, end[is.na(end)] <- start[is.na(end)])
-udat[c('start', 'end')] <- lapply(udat[c('start', 'end')], as.hexmode)
-
-# we rely on EastAsianWidth correctly representing emoji presentation
-# by marking the relevant code points as W or F (some emojis default to
-# emoji presentation, but others don't).
-
-eaw <- c(N=1L, Na=1L, W=2L, F=2L, H=1L, A=1L)
-
-with(udat,
-  stopifnot(
-    all(V2 %in% names(eaw)),
-    all(start <= end),
-    all(diff(start) > 0),
-    all(diff(end) > 0)
-  )
-)
 # We should probably collapse ranges that are contiguous and have the
 # same width property as we are not currently using any of the other
 # properties that cause ranges to be split.
 
-# - Parse R EAW ----------------------------------------------------------------
-
-# Parse the R data that exists, look for `table_wcwidth` and the next table
-# after that, and process the data in between
-
-parse_rlocale <- function(rlocale) {
-  raw <- readLines(rlocale)
-  rlstr <- grep('static +const +struct +interval_wcwidth +table_wcwidth\\[\\]', raw)
-  rlend <- grep('static +const +struct +interval +table_walpha\\[\\]', raw)
-  stopifnot(length(rlend) == 1, length(rlstr) == 1, rlstr + 1 < rlend)
-  txt1 <- raw[(rlstr + 1):rlend]
-
-  # Either we have a data row, or we have a conditional row, or a comment row
-
-  pbase <- paste0(
-    "^\\s*\\{0x([0-9A-Fa-f]{2,6}),0x([0-9A-Fa-f]{2,6})(,\\{[012](?:,[012]){6}\\})\\},",
-    "(\\s*(?://.*|/\\*.*\\*/|))\\s*$"
-  )
-  pwin <- "^#ifdef\\s*Win32"
-  pwinelse <- "^#else\\s*"
-  pwinend <- "^#endif\\s*"
-  pcomm <- "^\\s*//"
-
-  lbase <- grep(pbase, txt1)
-  lwin <- grep(pwin, txt1)
-  lwinelse <- grep(pwinelse, txt1)
-  lwinend <- grep(pwinend, txt1)
-  lcomm <- grep(pcomm, txt1)
-
-  # We should have matched every line in the table with these patterns,
-  # and ifdefs els should all be one apart from each other, all should
-  # be if/else/end, and none should be contiguous.  This allows us to
-  # make helpful assumptions later, such as we can't be adding a new
-  # unicode point inside an ifdef block.  If some of these assumptions
-  # change a lot of code will need to change.
-
-  stopifnot(
-    all.equal(
-      sort(c(lbase, lwin, lwinelse, lwinend, lcomm)),
-      seq(1L, rlend - rlstr - 5, by=1)
-    ),
-    txt1[rlend - rlstr - 4] == "};",
-    length(lwin) == length(lwinelse),
-    length(lwin) == length(lwinend),
-    all((lwinelse - lwin) == 2L),
-    all((lwinend - lwinelse) == 2L)
-  )
-  isifdef <- rep(0:4, length(lwin)) + rep(lwin, each=5)
-
-  # Extract data into columns, we'll have duplicate ranges because
-  # of the ifdefs; that should be fine.
-
-  txt2 <- txt1[lbase]
-  matches <- regexec(pbase, txt2, perl=TRUE)
-  txt3 <- regmatches(txt2, matches)
-
-  stopifnot(all(lengths(txt3) == 5))
-
-  rdat <- as.data.frame(t(matrix(unlist(txt3), 5)[2:5,]), stringsAsFactors=FALSE)
-  names(rdat) <- c('start', 'end', 'widths', 'comment')
-
-  wm <- gregexpr("[0-9]+", rdat$widths)
-  wi <- regmatches(rdat$widths, wm)
-
-  stopifnot(all(lengths(wi) == 7))
-  wil <- data.frame(
-    matrix(unlist(wi), ncol=7, byrow=TRUE), stringsAsFactors=FALSE
-  )
-  rdat <- cbind(rdat, wil)
-  rdat[c('start', 'end')] <- lapply(rdat[c('start', 'end')], as.hexmode)
-  rdat[['rid_raw']] <- lbase
-
-  with(rdat,
-    stopifnot(
-      all(start <= end),
-      # duplicates possible due of ifdefs producing different tables for
-      # Win32...
-      all(diff(as.integer(start)) >= 0),
-      all(diff(as.integer(end)) >= 0)
-    )
-  )
-  list(
-    lbase=lbase, lwin=lwin, txt1=txt1, isifdef=isifdef,
-    lwin=lwin, txt2=txt2, matches=matches, rdat=rdat,
-    rlstr=rlstr, rlend=rlend, raw=raw
-  )
-}
 ldat <- parse_rlocale('rlocale_data.h')
 
 # originally the stuff in ldat was generated in the script and
@@ -151,17 +32,6 @@ list2env(ldat, environment())
 # Generate a mapping of R's entries to unicode entries; need to
 # handle fact there are duplicate R entries due to ifdef statements
 
-allpoints <- seq(0L, max(c(udat[['end']], rdat[['end']])))
-all_points <- function(dat, prefix) {
-  sizes <- with(dat, end - start + 1L)
-  allp <- with(
-    dat,
-    data.frame(
-      id=rep(start - 1L, sizes) + sequence(sizes),
-      id2=rep(seq_len(nrow(dat)), sizes)
-  ) )
-  setNames(allp, c('id', paste0(prefix, 'id')))
-}
 rallp <- all_points(rdat, 'r')
 uallp <- all_points(udat, 'u')
 map <- merge(rallp, uallp, by='id', all=TRUE)
@@ -390,7 +260,8 @@ txtres <- txtres[order(lnstarts)]
 # and into final file
 
 txtfin <- c(raw[seq_len(rlstr)], txtres, raw[-seq_len(rlend)])
-writeLines(txtfin, 'rlocale_data2.h')
+message('skipping writing file')
+# writeLines(txtfin, 'rlocale_data2.h')
 
 # - Sanity Checks --------------------------------------------------------------
 
@@ -427,60 +298,3 @@ stopifnot(
   identical(rallp2a$id, rallp2b$id),
   all(rallp$id %in% rallp2a$id)
 )
-# Generate every code point from 0x1 to 0x10fffd
-
-start <- as.character(
-  parse(
-    text=paste0(
-      "c(",
-      paste0(
-        "'\\u", as.character(as.hexmode(1:0xa0)), "'",
-        collapse=",\n"
-      ),
-      ",NULL)"
-    ),
-    keep.source=FALSE
-  )[[1]][-1]
-)
-start <- start[-length(start)]
-end <- as.character(
-  parse(
-    text=paste0(
-      "c(",
-      paste0(
-        "'\\U", as.character(as.hexmode(0xa1:0x10fffd), width=8), "'",
-        collapse=",\n"
-      ),
-      ",NULL)"
-    ),
-    keep.source=FALSE
-  )[[1]][-1]
-)
-end <- end[-length(end)]
-cps <- c(start, end)
-
-
-# - Comparisons ----------------------------------------------------------------
-#
-# Compare to other sources:
-#
-# * glibc 2.29 (Unicode 11?)
-# * utf8::utf8_width
-# * stri::stri_width
-# * R 3.6.3
-
-cpsa <- cps
-cpsa[0xd800:0xdFFF] <- ""  # surrogates that stringi doesn't like
-
-glibc <- readLines('glibc_widths')
-utf8 <- utf8::utf8_width(cps)
-stri <- stringi::stri_width(cpsa)
-r3.6 <- nchar(cps, type='width')
-r4.0 <- readRDS('r40widths.rds')
-
-dat <- data.frame(
-  cp=1:0x10fffd,
-  r3.6, r4.0, glibc, utf8, stri
-)
-dat[['cp']] <- as.hexmode(dat[['cp']])
-subset(dat, r3.6 != r4.0)[1:50,]
---
+++ process-lib.R
@@ -0,0 +1,142 @@
+# - Parse R EAW ----------------------------------------------------------------
+
+# Parse the R data that exists, look for `table_wcwidth` and the next table
+# after that, and process the data in between
+
+options(stringsAsFactors=FALSE)
+
+parse_rlocale <- function(rlocale) {
+  raw <- readLines(rlocale)
+  rlstr <- grep('static +const +struct +interval_wcwidth +table_wcwidth\\[\\]', raw)
+  rlend <- grep('static +const +struct +interval +table_walpha\\[\\]', raw)
+  stopifnot(length(rlend) == 1, length(rlstr) == 1, rlstr + 1 < rlend)
+  txt1 <- raw[(rlstr + 1):rlend]
+
+  # Either we have a data row, or we have a conditional row, or a comment row
+
+  pbase <- paste0(
+    "^\\s*\\{0x([0-9A-Fa-f]{2,6}),0x([0-9A-Fa-f]{2,6})(,\\{[012](?:,[012]){6}\\})\\},",
+    "(\\s*(?://.*|/\\*.*\\*/|))\\s*$"
+  )
+  pwin <- "^#ifdef\\s*Win32"
+  pwinelse <- "^#else\\s*"
+  pwinend <- "^#endif\\s*"
+  pcomm <- "^\\s*//"
+
+  lbase <- grep(pbase, txt1)
+  lwin <- grep(pwin, txt1)
+  lwinelse <- grep(pwinelse, txt1)
+  lwinend <- grep(pwinend, txt1)
+  lcomm <- grep(pcomm, txt1)
+
+  # We should have matched every line in the table with these patterns,
+  # and ifdefs els should all be one apart from each other, all should
+  # be if/else/end, and none should be contiguous.  This allows us to
+  # make helpful assumptions later, such as we can't be adding a new
+  # unicode point inside an ifdef block.  If some of these assumptions
+  # change a lot of code will need to change.
+
+  stopifnot(
+    all.equal(
+      sort(c(lbase, lwin, lwinelse, lwinend, lcomm)),
+      seq(1L, rlend - rlstr - 5, by=1)
+    ),
+    txt1[rlend - rlstr - 4] == "};",
+    length(lwin) == length(lwinelse),
+    length(lwin) == length(lwinend),
+    all((lwinelse - lwin) == 2L),
+    all((lwinend - lwinelse) == 2L)
+  )
+  isifdef <- rep(0:4, length(lwin)) + rep(lwin, each=5)
+
+  # Extract data into columns, we'll have duplicate ranges because
+  # of the ifdefs; that should be fine.
+
+  txt2 <- txt1[lbase]
+  matches <- regexec(pbase, txt2, perl=TRUE)
+  txt3 <- regmatches(txt2, matches)
+
+  stopifnot(all(lengths(txt3) == 5))
+
+  rdat <- as.data.frame(t(matrix(unlist(txt3), 5)[2:5,]), stringsAsFactors=FALSE)
+  names(rdat) <- c('start', 'end', 'widths', 'comment')
+
+  wm <- gregexpr("[0-9]+", rdat$widths)
+  wi <- regmatches(rdat$widths, wm)
+
+  stopifnot(all(lengths(wi) == 7))
+  wil <- data.frame(
+    matrix(unlist(wi), ncol=7, byrow=TRUE), stringsAsFactors=FALSE
+  )
+  rdat <- cbind(rdat, wil)
+  rdat[c('start', 'end')] <- lapply(rdat[c('start', 'end')], as.hexmode)
+  rdat[['rid_raw']] <- lbase
+
+  with(rdat,
+    stopifnot(
+      all(start <= end),
+      # duplicates possible due of ifdefs producing different tables for
+      # Win32...
+      all(diff(as.integer(start)) >= 0),
+      all(diff(as.integer(end)) >= 0)
+    )
+  )
+  list(
+    lbase=lbase, lwin=lwin, txt1=txt1, isifdef=isifdef,
+    lwin=lwin, txt2=txt2, matches=matches, rdat=rdat,
+    rlstr=rlstr, rlend=rlend, raw=raw
+  )
+}
+# - Expand Points -------------------------------------------------------------
+
+all_points <- function(dat, prefix) {
+  sizes <- with(dat, end - start + 1L)
+  allp <- with(
+    dat,
+    data.frame(
+      id=rep(start - 1L, sizes) + sequence(sizes),
+      id2=rep(seq_len(nrow(dat)), sizes)
+  ) )
+  setNames(allp, c('id', paste0(prefix, 'id')))
+}
+# - Import Unicode EAW ---------------------------------------------------------
+
+uni_eaw <- function(eaw) {
+  udat <- read.delim(
+    stringsAsFactors = FALSE,
+    eaw,
+    comment.char = "#",
+    sep = ";",
+    strip.white = TRUE,
+    header = FALSE
+  )
+  # Generate and validate the ranges
+
+  points <- strsplit(udat[['V1']], '..', fixed=TRUE)
+  lens <- lengths(points)
+  stopifnot(all(lens > 0 & lens < 3))
+  for(i in seq_along(points)) length(points[[i]]) <- 2
+  points <- matrix(unlist(points), 2)
+
+  udat[['start']] <- points[1,]
+  udat[['end']] <- points[2,]
+  udat <- within(udat, end[is.na(end)] <- start[is.na(end)])
+  udat[c('start', 'end')] <- lapply(udat[c('start', 'end')], as.hexmode)
+
+  # we rely on EastAsianWidth correctly representing emoji presentation
+  # by marking the relevant code points as W or F (some emojis default to
+  # emoji presentation, but others don't).
+
+  eaw <- c(N=1L, Na=1L, W=2L, F=2L, H=1L, A=1L)
+
+  with(udat,
+    stopifnot(
+      all(V2 %in% names(eaw)),
+      all(start <= end),
+      all(diff(start) > 0),
+      all(diff(end) > 0)
+    )
+  )
+  udat
+}
+
